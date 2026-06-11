1d41a4427df75778d06f00bdd5a355accbd409d0
Manvendra Singh <manvendrask2@gmail.com>
2026-06-12 00:58:40 +0530

    feat: add E2E tests for ping handler

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"io"
+	"log/slog"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -10,7 +11,7 @@ import (
 	"snippetbox.manvendrask.com/internal/assert"
 )
 
-func TestPing(t *testing.T) {
+func TestPingUnit(t *testing.T) {
 	responseRecorder := httptest.NewRecorder()
 
 	request, err := http.NewRequest(http.MethodGet, "/", nil)
@@ -29,6 +30,30 @@ func TestPing(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	body = bytes.TrimSpace(body)
+	assert.Equal(t, string(body), "OK")
+}
+
+func TestPing(t *testing.T) {
+	app := &Application{
+		logger: slog.New(slog.NewTextHandler(io.Discard, nil)),
+	}
+
+	testServer := httptest.NewTLSServer(app.routes())
+	defer testServer.Close()
+
+	resp, err := testServer.Client().Get(testServer.URL + "/ping")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, resp.StatusCode, http.StatusOK)
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
 	body = bytes.TrimSpace(body)
 	assert.Equal(t, string(body), "OK")
 }
