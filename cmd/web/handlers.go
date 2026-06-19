0f7c15fd2ef3ca93b58d7a9258cd6a484eb1fb3d
Manvendra Singh <manvendrask2@gmail.com>
2026-06-20 00:24:04 +0530

    feat: add account page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -227,3 +227,22 @@ func (app *Application) about(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
 	app.render(w, r, http.StatusOK, "about.tmpl.html", data)
 }
+
+func (app *Application) accountView(w http.ResponseWriter, r *http.Request) {
+	userID := app.sessionManager.GetInt(r.Context(), "authenticatedUserID")
+
+	user, err := app.users.Get(userID)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+		} else {
+			app.serverError(w, r, err)
+		}
+		return
+	}
+
+	data := app.newTemplateData(r)
+	data.User = user
+
+	app.render(w, r, http.StatusOK, "account.tmpl.html", data)
+}
